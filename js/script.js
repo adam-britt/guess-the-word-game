16f9bf853c2cee84be6a8e03f5ee8051516bf99f
Adam Britt <adamrussellbritt@gmail.com>
2023-09-18 11:05:41 -0500

    js changes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const guessedLetters = ".guessed-letters";      // prior letters guessed
-const letterInput = "#letter";                    // current letter guessed
-const guessButton = ".guess";                   // guess button
+const letterInput = "#letter";                  // current letter guessed
+const guessLetterButton = ".guess";             // guess button
 const wordInProgress = ".word-in-progress";     // word in progress
 const numGuessesRemaining = ".remaining";       // guesses remaining sentence
 const numGuessesSpan = ".remaining span";       // guesses remaining #
@@ -8,32 +8,36 @@ const lettersGuessed = ".message";              // letters guessed message
 const playAgainButton = ".play-again";          // play again button
 const word = "magnolia";                        // word to guess
 
-// Display circles as placeholders for the chosen word's letters
+// Display circles as placeholders for chosen word letters
 const placeholder = function (word) {
-
+    // Create array of letters
     const placeholderLetters = [];
-
+    // For each letter that exists, initially, add a ● to the array
     for(const letter of word) {
         console.log(letter);
         placeholderLetters.push(`●`);
     }
-    
+    // Join all placeholder letters to form word in progress
     wordInProgress.innerText = placeholderLetters.join("");
 };
-
+// Call function
 placeholder(word);
 
-
-
-
-
-guessButton.addEventListener("click", function (e) {
-    // prevent default reload since we're working with a dynamic form
+// Guess button
+guessLetterButton.addEventListener("click", function (e) {
+    // Prevent default reload since we're working with a dynamic form
     e.preventDefault();
 
-    // create variable to capture input value
+    // Create variable to capture input value
     const guess = letterInput.value;
     console.log(guess);
     letterInput.value = "";
-
+    
+    
 });
+
+
+
+const isItALetter = function (userInput) {
+    
+}
